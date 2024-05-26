fbc4e86ffe5f531ea56f6b479708005f4d2bfc29
luk3mn <lucasrenan2015@gmail.com>
2024-05-26 10:21:27 -0300

    config: project initialzer

---
+++ src/main/java/com/luke/anitrail/AnitrailApplication.java
@@ -0,0 +1,13 @@
+package com.luke.anitrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnitrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnitrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luke/anitrail/AnitrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luke.anitrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnitrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
